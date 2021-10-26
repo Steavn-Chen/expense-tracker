87c823169b6fb236ac58b6fe71a7a9eaf48006ef
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-27 01:42:41 +0800

    建立使用者註冊功能

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post("/login", (req, res) => {
-  res.render("login")
+router.post('/login', (req, res) => {
+  res.render('login')
 })
 
-router.get("/register", (req, res) => {
-  res.render("register");
-});
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
-router.post("/register", (req, res) => {
-  res.render("register");
-});
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+  .then(user => {
+    if (user) {
+      console.log('此帳號己註冊')
+      res.render('register', { name, email, password, confirmPassword })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    }
+  })
+})
 
 
 module.exports = router
