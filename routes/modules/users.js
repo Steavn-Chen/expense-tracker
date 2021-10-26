3801a9e1d7ac527048536b090dd81c023b7dc523
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-27 01:02:11 +0800

    建立使用者登入頁面

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,8 +5,17 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post("login", (req, res) => {
+router.post("/login", (req, res) => {
   res.render("login")
 })
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  res.render("register");
+});
+
 
 module.exports = router
