67b67cac504ccc10a884820559684fb9e786d19d
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-27 13:25:46 +0800

    建立 bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -35,20 +36,42 @@ router.post('/register', (req, res) => {
     errors
     })
   }
-    User.findOne({ email }).then(user => { 
-      if (user) {
-        errors.push({ message: '這個電子郵件地址己被註冊。'})
-        return res.render('register', { name, email, password, confirmPassword, errors });
-      } else {
-        return User.create({
-          name,
-          email,
-          password,
-        })
-          .then(() => res.redirect('/'))
-          .catch((err) => console.log(err));
-      }
-    });
+   User.findOne({ email }).then((user) => {
+     if (user) {
+       errors.push({ message: "這個電子郵件地址己被註冊。" });
+       return res.render("register", {
+         name,
+         email,
+         password,
+         confirmPassword,
+         errors,
+      })
+     } 
+     return bcrypt
+       .genSalt(10)
+       .then(salt => bcrypt.hash(password, salt))
+       .then(hash => User.create({
+         name,
+         email,
+         password: hash
+       }))
+       .then(() => res.redirect('/user/login'))
+       .catch(err => console.log(err)) 
+   })
+    // User.findOne({ email }).then(user => { 
+    //   if (user) {
+    //     errors.push({ message: '這個電子郵件地址己被註冊。'})
+    //     return res.render('register', { name, email, password, confirmPassword, errors });
+    //   } else {
+    //     return User.create({
+    //       name,
+    //       email,
+    //       password,
+    //     })
+    //       .then(() => res.redirect('/'))
+    //       .catch((err) => console.log(err));
+    //   }
+    // })
 })
 
 router.get('/logout', (req, res) => {
