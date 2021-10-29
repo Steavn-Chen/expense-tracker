5d61b2e08d8a2e6d4afb5610ecaa0c3b15282c50
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-29 23:16:06 +0800

    修正 passport.js 反序列化錯誤

--- config/passport.js
+++ config/passport.js
@@ -31,24 +31,21 @@ module.exports = app => {
       .catch((err) => done(err, null));
   }))
   
-  passport.use(
-    new FacebookStrategy(
-      {
+  passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
         callbackURL: process.env.FACEBOOK_CALLBACK,
-        profileFields: ["email", "displayName"],
-      },
-      (accessToken, refreshToken, profile, done) => {
+        profileFields: ['email', 'displayName']
+      }, (accessToken, refreshToken, profile, done) => {
         const { name, email } = profile._json
-        User.findOne({ email }).then((user) => {
-          if (user) return done(null, user);
+        User.findOne({ email })
+        .then((user) => {
+          if (user) return done(null, user)
           const randomPassword = Math.random().toString(36).slice(-8)
           bcrypt
             .genSalt(10)
             .then((salt) => bcrypt.hash(randomPassword, salt))
-            .then((hash) =>
-              User.create({
+            .then((hash) => User.create({
                 name,
                 email,
                 password: hash
@@ -92,7 +89,7 @@ module.exports = app => {
   )
 
   passport.serializeUser((user, done) => {
-    return done(null, user.id)
+     done(null, user.id)
   })
 
   passport.deserializeUser((id, done) => {
