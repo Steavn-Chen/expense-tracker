89a25ad36296e1089679243904542b94ffb921e0
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-30 01:35:47 +0800

    修正passport無法登入facebook

--- config/passport.js
+++ config/passport.js
@@ -31,33 +31,57 @@ module.exports = app => {
       .catch((err) => done(err, null));
   }))
 
+  // passport.use(new FacebookStrategy({
+  //       clientID: process.env.FACEBOOK_ID,
+  //       clientSecret: process.env.FACEBOOK_SECRET,
+  //       callbackURL: process.env.FACEBOOK_CALLBACK,
+  //       profileFields: ['email', 'displayName']
+  //     }, (accessToken, refreshToken, profile, done) => {
+  //       const { name, email } = profile._json
+  //       User.findOne({ email })
+  //       .then((user) => {
+  //         if (user) return done(null, user)
+  //         const randomPassword = Math.random().toString(36).slice(-8)
+  //         bcrypt
+  //           .genSalt(10)
+  //           .then((salt) => bcrypt.hash(randomPassword, salt))
+  //           .then((hash) => User.create({
+  //               name,
+  //               email,
+  //               password: hash
+  //             })
+  //           )
+  //           .then(user => done(null, user))
+  //           .catch((err) => done(err, false))
+  //       })
+  //     }
+  //   )
+  // )
   passport.use(new FacebookStrategy({
-        clientID: process.env.FACEBOOK_ID,
-        clientSecret: process.env.FACEBOOK_SECRET,
-        callbackURL: process.env.FACEBOOK_CALLBACK,
-        profileFields: ['email', 'displayName']
-      }, (accessToken, refreshToken, profile, done) => {
-        const { name, email } = profile._json
-        User.findOne({ email })
-        .then((user) => {
-          if (user) return done(null, user)
-          const randomPassword = Math.random().toString(36).slice(-8)
-          bcrypt
-            .genSalt(10)
-            .then((salt) => bcrypt.hash(randomPassword, salt))
-            .then((hash) => User.create({
-                name,
-                email,
-                password: hash
-              })
-            )
-            .then(user => done(null, user))
-            .catch((err) => done(err, false))
-        })
-      }
-    )
-  )
-
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+    .then(user => {
+      if (user) return done(null, user)
+      const randomPassword = Math.random().toString(36).slice(-8)
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(randomPassword, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(user => done(null, user))
+        .catch(err => done(err, false))
+    })
+  }
+  ))
+  
   passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
